8f3ed086c2290c392f0d9759b0949538c71567fd
diablomike20 <73985391+diablomike20@users.noreply.github.com>
2026-08-21 23:40:02 +0200

    Update board.h

--- trunk/configs/boards/BELKIN/BN750DB/board.h
+++ trunk/configs/boards/BELKIN/BN750DB/board.h
@@ -1,7 +1,7 @@
 /* Belkin N750 DB */
 
 #define BOARD_PID		"BN750DB"
-#define BOARD_NAME		"BL-N750DB"
+#define BOARD_NAME		"BN750DB"
 #define BOARD_DESC		"Belkin N750 DB Wireless Router"
 #define BOARD_VENDOR_NAME	"Belkin International Inc."
 #define BOARD_VENDOR_URL	"http://www.belkin.com/us/"
